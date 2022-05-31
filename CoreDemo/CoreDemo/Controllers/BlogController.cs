9c24d25a5cbc06a835b2af794f55ac374ef88a16
nazlibuse <nazli.buse44@gmail.com>
2022-05-31 11:32:55 +0300

    updated

--- CoreDemo/CoreDemo/Controllers/BlogController.cs
+++ CoreDemo/CoreDemo/Controllers/BlogController.cs
@@ -13,8 +13,14 @@ namespace CoreDemo.Controllers
         BlogManager bm = new BlogManager(new EFBlogRepository());
         public IActionResult Index()
         {
-            var values = bm.GetList();
+            var values = bm.GetBlogListWithCategory();
             return View(values);
         }
+        public IActionResult BlogReadAll(int id)
+        {
+            var values = bm.GetBlogByID(id);
+            return View();
+        }
+
     }
 }
